2bb3a024eacdd59b5f829d6030482c7cd6e39dc3
ssandall <suriyasandall@gmail.com>
2018-07-11 21:38:02 +1000

    NEW: Added Javascript add.js file and implemented in device-list.html

---
+++ app.js
@@ -0,0 +1,4 @@
+const devices = [];
+devices.push({ user: "Mary", name: "Mary's iPhone" });
+devices.push({ user: "Alex", name: "Alex's Surface Pro" });
+devices.push({ user: "Mary", name: "Mary's MacBook" });
