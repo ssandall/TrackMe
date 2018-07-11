148fdffd6ef5acc29d492c152ad1f7b3b3e9f077
ssandall <suriyasandall@gmail.com>
2018-07-11 22:06:40 +1000

    NEW: Applied bootstrap styling to device-list webpage

--- app.js
+++ app.js
@@ -2,3 +2,12 @@ const devices = [];
 devices.push({ user: "Mary", name: "Mary's iPhone" });
 devices.push({ user: "Alex", name: "Alex's Surface Pro" });
 devices.push({ user: "Mary", name: "Mary's MacBook" });
+
+devices.forEach(function(device) {
+    $('#devices tbody').append(`
+    <tr>
+    <td>${device.user}</td>
+    <td>${device.name}</td>
+    </tr>`
+    );
+   });
