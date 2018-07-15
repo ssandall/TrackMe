986baa64fb090a2b0020a82edb336b1e5f6a1221
ssandall <suriyasandall@gmail.com>
2018-07-16 01:06:32 +1000

    NEW: added routes for webpages

--- Web/server.js
+++ Web/server.js
@@ -8,6 +8,14 @@ app.get('/', (req, res) => {
     res.sendFile(`${base}/device-list.html`);
 });
 
+app.get('/register-device', (req, res) => {
+    res.sendFile(`${base}/register-device.html`);
+});
+
+app.get('/send-command', (req, res) => {
+    res.sendFile(`${base}/send-command.html`);
+});
+
 app.get('*', (req, res) => {
     res.sendFile(`${base}/404.html`);
 });
