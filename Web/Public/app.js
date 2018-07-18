1b2a3493769bf8a1ad0e07380a3345d2e3de7589
ssandall <suriyasandall@gmail.com>
2018-07-18 13:47:07 +1000

    FIX: Fixed Navbar issue changed to jquery.min instead of jquery.slim

--- Web/Public/app.js
+++ Web/Public/app.js
@@ -1,7 +1,7 @@
 const devices = JSON.parse(localStorage.getItem('devices')) || [];
-const users = JSON.parse(localStorage.getItem('users')) || [];
-
-$('#navbar').load('navbar.html');
+devices.push({ user: "Mary", name: "Mary's iPhone" });
+devices.push({ user: "Alex", name: "Alex's Surface Pro" });
+devices.push({ user: "Mary", name: "Mary's MacBook" });
 
 devices.forEach(function(device) {
     $('#devices tbody').append(`
@@ -10,40 +10,15 @@ devices.forEach(function(device) {
     <td>${device.name}</td>
     </tr>`
     );
-});
+   });
  
 $('#add-device').on('click', function() { 
     const user = $('#user').val();
     const name = $('#name').val();
     devices.push({ user, name });
     localStorage.setItem('devices', JSON.stringify(devices));
-    location.href = '/device-list';
-});
-
-$('#login').on('click', function() { 
-    const username = $('#username').val();
-    const password = $('#password').val();
-});
-
-$('#register-account').on('click', function() {
-    const userName = $('#username').val();
-    const password = $('#password').val();
-    const confPassword = $('#passwordConfirm').val()
-    const exists = users.find((user => user.name === userName));
-        if (exists){
-            $('#messageError').append('<p class="alert alert-danger" style="font-style: italic" >User already exists. Click <a href="/login">here</a> to login. </p>');
-
-        }
-        else if (password !== confPassword){
-            $('#passwordError').append('<p class="alert alert-danger" style="font-style: italic"> ERROR: Password does not match </p>')
-        }
-        else
-        {
-            console.log('created user')
-            users.push({name: userName, password: confPassword})
-            localStorage.setItem('users', JSON.stringify(users));
-            location.href = "/login";
-        }
+    location.href = 'device-list.html'
+    console.log(devices);
 });
 
 $('#send-command').on('click', function() {
@@ -51,3 +26,6 @@ $('#send-command').on('click', function() {
     console.log(`command is: ${command}`);
 });
 
+$('#navbar').load('navbar.html',function(){
+    //console.log('NAVBAR');
+});
