579904849df64a4d56307659f9f04af7110d19f1
ssandall <suriyasandall@gmail.com>
2018-07-18 14:47:43 +1000

    UPDATE: commented out initial user push

--- Web/Public/app.js
+++ Web/Public/app.js
@@ -1,7 +1,7 @@
 const devices = JSON.parse(localStorage.getItem('devices')) || [];
-devices.push({ user: "Mary", name: "Mary's iPhone" });
-devices.push({ user: "Alex", name: "Alex's Surface Pro" });
-devices.push({ user: "Mary", name: "Mary's MacBook" });
+//devices.push({ user: "Mary", name: "Mary's iPhone" });
+//devices.push({ user: "Alex", name: "Alex's Surface Pro" });
+//devices.push({ user: "Mary", name: "Mary's MacBook" });
 
 devices.forEach(function(device) {
     $('#devices tbody').append(`
