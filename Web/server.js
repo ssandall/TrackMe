12b53fbcb9704fa9b070d1bff5f6bd73b12b47b0
Suriya Sandall <suriya@10-140-1-113.wifi-m.deakin.edu.au>
2018-08-02 18:42:29 +1000

    UPDATE: Updating the file

--- Web/server.js
+++ Web/server.js
@@ -1,46 +1,45 @@
-const express = require('express');
-const app = express();
-const port = 3000; 
-const base = `${__dirname}/Public`;
-app.use(express.static('Public'));
+const express = require('express')
+const app = express()
+const port = 3000
+const base = `${__dirname}/Public`
+app.use(express.static('Public'))
 
 app.get('/', (req, res) => {
-    res.sendFile(`${base}/device-list.html`);
-});
+  res.sendFile(`${base}/device-list.html`)
+})
 
 app.get('/register-device', (req, res) => {
-    res.sendFile(`${base}/register-device.html`);
-});
+  res.sendFile(`${base}/register-device.html`)
+})
 
 app.get('/send-command', (req, res) => {
-    res.sendFile(`${base}/send-command.html`);
-});
+  res.sendFile(`${base}/send-command.html`)
+})
 
 app.get('/device-list', (req, res) => {
-    res.sendFile(`${base}/device-list.html`);
-});
+  res.sendFile(`${base}/device-list.html`)
+})
 
 app.get('/about-me', (req, res) => {
-    res.sendFile(`${base}/about-me.html`);
-});
+  res.sendFile(`${base}/about-me.html`)
+})
 
 app.get('/login', (req, res) => {
-    res.sendFile(`${base}/login.html`);
-});
+  res.sendFile(`${base}/login.html`)
+})
 
 app.get('/registration', (req, res) => {
-    res.sendFile(`${base}/registration.html`);
-});
+  res.sendFile(`${base}/registration.html`)
+})
 
 app.get('/device-history', (req, res) => {
-    res.sendFile(`${base}/device-history.html`);
-});
+  res.sendFile(`${base}/device-history.html`)
+})
 
 app.get('*', (req, res) => {
-    res.sendFile(`${base}/404.html`);
-});
+  res.sendFile(`${base}/404.html`)
+})
 
 app.listen(port, () => {
-    console.log(`listening on port ${port}`);
-});
-
+  console.log(`listening on port ${port}`)
+})
